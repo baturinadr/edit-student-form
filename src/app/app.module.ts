d2083668805e16ef17597c0416437559ce1f464f
Andrew Baturin <baturin.dev@gmail.com>
2021-11-03 19:25:27 +0200

    Added Ng-Bootstrap UIkit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 // Components
 import {AppComponent} from './app.component';
@@ -10,7 +11,8 @@ import {AppComponent} from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
