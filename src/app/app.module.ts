176a522c9b5ddc739828741ac1a345df8d3700f1
Andrew Baturin <baturin.dev@gmail.com>
2021-11-04 21:30:38 +0200

    Created template for application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from "@angular/forms";
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 // Components
 import {AppComponent} from './app.component';
+import {AchievementComponent} from './components/achievement/achievement.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AchievementComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     NgbModule
   ],
   providers: [],
